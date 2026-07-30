889134439db9d6f78116a6fe16e0506964f433cd
unknown <Asaad-E>
2026-07-30 14:33:16 -0500

    first commint

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Raylib_cs;
+
+namespace TerrariaDemo;
+
+class Program
+{
+    
+    static void Main()
+    {
+        Raylib.InitWindow(720, 480, "Test");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            // New Frame
+            float deltaTime = Raylib.GetFrameTime();
+
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+
+            Raylib.DrawText($"FPS: {1/deltaTime:F2}", 20, 20, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+    }
+}
